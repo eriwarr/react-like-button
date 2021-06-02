d6f982e55a259c84fbd744180e401e5afec48f55
Eric Warren II <ericwarrenii@Erics-MacBook-Pro.local>
2021-06-02 09:09:13 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
+import { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      numberOfTimesClicked: 1,
+    }
+    this.countClicks = this.countClicks.bind(this);
+    }
+
+  countClicks(event){
+   this.setState({numberOfTimesClicked: this.state.numberOfTimesClicked + 1 })
+   if(this.state.numberOfTimesClicked === 1) {
+     event.target.innerHTML = `${this.state.numberOfTimesClicked} like`;
+   } else {
+     event.target.innerHTML = `${this.state.numberOfTimesClicked} likes`;
+   }
+
+  }
+
+  render() {
+    return (
+        <div className="App-header">
+        <button className="Button" onClick={this.countClicks}> 0 likes</button>
+        </div>
+    )
+  }
 }
 
 export default App;
